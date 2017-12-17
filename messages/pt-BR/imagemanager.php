4f27fe15840bdc9cf916f262cdaef31bea724535
Guga <gugatxr@gmail.com>
2017-12-17 19:10:55 -0200

    add brazilian translation

---
+++ messages/pt-BR/imagemanager.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'Are you sure you want to delete this image?' => 'Você tem certeza que gostaria de excluir a imagem?',
+    'Are you sure you want to detach the image?' => 'Você tem certeza que gostaria de desanexar a imagem?',
+    'Cancel' => 'Cancelar',
+    'Created' => 'Criado',
+    'Crop' => 'Cortar',
+    'Crop and select' => 'Cortar e Selecionar',
+    'Delete' => 'Deletar',
+    'File hash' => 'Hash do Arquivo',
+    'File name' => 'Nome do Arquivo',
+    'Modified' => 'Modificado',
+    'Search' => 'Pesquisar',
+    'Select' => 'Selecionar',
+    'Image manager' => 'Gerenciador de Imagens',
+    'Upload' => 'Upload'
+];
